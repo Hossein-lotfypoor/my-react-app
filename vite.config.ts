6ce4714db7e4bd687d53a8c03ab6e77bf2e5ece2
Hossein Lotfypoor <148619164+Hossein-lotfypoor@users.noreply.github.com>
2026-02-27 08:09:40 +0330

    fixed the base path for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // دقت کن: بین دو اسلش و حروف کوچک
+  base: "/my-react-app/", 
   plugins: [
     react(),
     tailwindcss()
-
   ],
 })
